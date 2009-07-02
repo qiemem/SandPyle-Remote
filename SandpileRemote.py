12bf43acbff3564a6cf6c839ff8df852bd36a66f
Bryan Head <bryan.head@gmail.com>
2009-07-02 14:05:45 -0700

    Fixed bugs. In particular, started using the file form of a socket to receive messages instead of the recv method of the socket. This guarantees that the script receives the full message before returning by reading until it sees a newline. This was a big problem before.

--- SandpileRemote.py
+++ SandpileRemote.py
@@ -9,6 +9,12 @@ class CommandError(Exception):
 class SandpileRemote:
     def __init__(self):
         self.autoRepaint = True
+        self.verbose = False
+        self.echo = False
+
+    def printVerbose(self, msg):
+        if self.verbose:
+            print(msg)
 
     def checkResult(self, result):
         if(result != "done\n"):
@@ -20,35 +26,65 @@ class SandpileRemote:
 
     def connect(self, host="localhost", port=7236):
         self.s = socket()
+        self.printVerbose("Attempting to connect")
         self.s.connect((host, port))
+        self.printVerbose("Connected")
+        self.f = self.s.makefile()
 
     def close(self):
         self.s.close()
+        self.f.close()
+
+    def send(self,msg):
+        if self.echo:
+            self.printVerbose("Sending message: \"" + msg +"\"")
+        else:
+            self.printVerbose("Sending message")
+        self.s.send(msg+"\n")
+        self.printVerbose("Message sent")
+
+    def receive(self):
+        self.printVerbose("Waiting for message")
+        msg = self.f.readline()
+        if self.echo:
+            self.printVerbose("Received message: \"" + msg +"\"")
+        else:
+            self.printVerbose("Received message")
+        return msg
 
     def repaint(self):
-        self.s.send("repaint\n")
-        self.checkResult(self.s.recv(2048))
+        self.send("repaint")
+        self.checkResult(self.receive())
 
     def update(self):
-        self.s.send("update\n")
-        self.checkResult(self.s.recv(2048))
+        self.send("update")
+        self.checkResult(self.receive())
         self.tryRepaint()
 
     def stabilize(self):
-        self.s.send("stabilize\n")
-        self.checkResult(self.s.recv(2048))
+        self.send("stabilize")
+        self.checkResult(self.receive())
         self.tryRepaint()
 
     def deleteGraph(self):
-        self.s.send("delete_graph\n")
-        self.checkResult(self.s.recv(2048))
+        self.send("delete_graph")
+        self.checkResult(self.receive())
 
     def clearSand(self):
-        self.s.send("clear_sand")
-        self.checkResult(self.s.recv(2048))
+        self.send("clear_sand")
+        self.checkResult(self.receive())
 
     def getVertices(self):
-        self.s.send("get_vertices")
+        self.send("get_vertices")
+        vertexData = self.receive()
+        if vertexData == "\n":
+            return []
+        else:
+            return map(lambda x : map(float, x.split(" ")), vertexData.split(","))
+
+    def getEdge(self):
+        self.send("get_edges")
+        edgeData = self.receive()
         
 
 
